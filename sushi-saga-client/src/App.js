4c56551cd8a56e3f72f0971ed7150149353abcb9
DanBehrman <dan.behrman@wework.com>
2019-10-11 14:52:15 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,46 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushi: [],
+    eatenSushi: [],
+    startIndex: 0,
+    money: 50
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({sushi: data})
+    })
+  }
+
+  moreSushiHandler = () => {
+    let newObj = {...this.state}
+    newObj.startIndex = newObj.startIndex + 4
+    console.log(newObj)
+    this.setState(newObj)
+  }
+
+  eatSushi = (e, price) => {
+    if (e.target.id && price <= this.state.money) {
+      const newObj = {...this.state}
+      const foundSushi = this.state.sushi.find((sush) => {
+        return sush.id === parseInt(e.target.id)
+      })
+      newObj.eatenSushi.push(foundSushi)
+      newObj.money = newObj.money - parseInt(price)
+      console.log(e.target)
+      this.setState(newObj)
+    }
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushi={this.state.sushi.slice(this.state.startIndex, this.state.startIndex + 4)} moreSushi={this.moreSushiHandler} eatenSushi={this.state.eatenSushi} eatSushi={this.eatSushi}/>
+        <Table eatenSushi={this.state.eatenSushi} money={this.state.money}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          props.sushi.map((sush) => {
+            return <Sushi key={sush.id} id={sush.id} name={sush.name} img_url={sush.img_url} price={sush.price} eatenSushi={props.eatenSushi} eatSushi={props.eatSushi}/>
+          })
         }
-        <MoreButton />
+        <MoreButton moreSushi={props.moreSushi}/>
       </div>
     </Fragment>
   )
